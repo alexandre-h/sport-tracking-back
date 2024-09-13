feed1f682ab27e8efaa57d8d6fe99fa4dd193c3e
alexandre-h <herole.alexandre@gmail.com>
2024-09-13 20:14:18 +0200

    template: Add seance template [SPB- 24] (#6)
    * template: Add seance template
    
    * template: Update create template
    
    ---------
    
    Co-authored-by: Alexandre Herole <alexandreherole@Alexandres-MacBook-Pro.local>

--- app/models/seance_template.rb
+++ app/models/seance_template.rb
@@ -1,4 +1,7 @@
 class SeanceTemplate < ApplicationRecord
   has_many :seances
+  belongs_to :user
 
+  validates :name, presence: true, uniqueness: true
+  validates :exercice, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   }
 
   resources :seances
+  resources :seance_templates
 end
